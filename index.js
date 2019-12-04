631a67d1b12200e3a4494f174196fb94e5d9d1b4
Barbara Mayfield <bmayfield1991@gmail.com>
2019-12-04 14:02:47 -0600

    Get req for specific user working

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ app.get("/api/users", (req, res) => {
 })
 
 app.get("/api/users/:id", (req, res) => {
-    //
+    db.findById(req.params.id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+        })
 })
 
 
