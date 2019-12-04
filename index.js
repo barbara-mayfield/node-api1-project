cf7dacce4f6abcc03289eb43183cb0bd13de8424
Barbara Mayfield <bmayfield1991@gmail.com>
2019-12-04 12:14:34 -0600

    Initial and users get req working

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // implement your API here
+const express = require("express");
+let db = require("./data/db");
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the Test API" })
+})
+
+app.get("/api/users", (req, res) => {
+    db.find()
+        .then(users => {
+        res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+        })
+})
+
+app.get("/api/users/:id", (req, res) => {
+    //
+})
+
+
+app.post("/api/users", (req, res) => {
+    if(!req.body.name || !req.body.bio) {
+        return res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+    }
+
+    const newUser = {
+        name: req.body.name,
+        bio: req.body.bio
+    }
+
+    if(user) {
+    db.push(newUser)
+    res.status(201).json(newUser)
+    } else {
+        res.status(404).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+})
+
+app.delete("/api/users/:id", (req, res) => {
+    const user = db.find(row => row.id === req.params.id)
+
+    if(user) {
+        db = db.filter(row => row.id !== req.params.id)
+        res.json(user)
+    } else {
+        res.status(404).json({ errorMessage: "Failed to delete, user not found!" })
+    }
+})
+
+const port = 8080
+const host = "127.0.0.1" 
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}`);
+})
